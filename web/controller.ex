b7e3ec8edec97c163fbd37f88197792fb5e3639f
Meck <yesmeck@gmail.com>
2015-01-15 02:16:11 +0800

    move helper function out of macro

--- web/controller.ex
+++ web/controller.ex
@@ -1,17 +1,21 @@
 defmodule Punky.Controller do
+  import Plug.Conn
+
   defmacro __using__(_options) do
     quote do
       use Phoenix.Controller
 
       import Punky.Router.Helpers
 
-      def sign_in(conn, user) do
-        put_session(conn, :user_id, user.id)
-      end
-
-      def sign_out(conn) do
-        put_session(conn, :user_id, nil)
-      end
+      import unquote(__MODULE__)
     end
+  end
+
+  def sign_in(conn, user) do
+    put_session(conn, :user_id, user.id)
+  end
+
+  def sign_out(conn) do
+    put_session(conn, :user_id, nil)
   end
 end
